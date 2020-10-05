bf55422f8a8d912e71e80fd9ba50935453cff882
aulia-adil <71262004+aulia-adil@users.noreply.github.com>
2020-10-05 13:08:20 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -2,7 +2,6 @@
 # Muhammad Aulia Adil
 # Mon Sep 28 22:03:27 WIB 2020
 # Tue 29 Sep 2020 11:02:39 AM WIB
-# PALSU BANGET SEKALI UBAH
 
 FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
 SHA="SHA256SUM"
